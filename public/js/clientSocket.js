4b62f5fa6e03eab4ede901904b8ee72bb9a48752
Weikun Liang <weikunl@andrew.cmu.edu>
2015-12-12 15:22:18 -0500

    port

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
-	var socket = io.connect('http://pictionary-weikunliang.rhcloud.com:50000');
+	var socket = io.connect('http://pictionary-weikunliang.rhcloud.com:8000');
 
 	var username;
 	var isArtist;
