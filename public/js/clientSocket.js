398cf864b0bfdcc8ff7ad7a5e02b99ec4ea6c09a
Weikun Liang <weikunl@andrew.cmu.edu>
2016-05-30 13:26:10 -0400

    missing digits game completed

--- public/js/clientSocket.js
+++ public/js/clientSocket.js
@@ -3,17 +3,102 @@ $(document).ready(function(){
 	// connects the socket
 	var socket = io.connect();
 
-	// pings the server every once in a while so that it doesn't disconnect
-	socket.on('ping', function(data){
-      socket.emit('pong', {beat: 1});
-    });
-
     // After the user enters an username and hits join
 	$('#join').click(function () {
-		username = $('#username').val(); // username of the player
-		console.log($("input[name=player]:checked").val()+" has joined");
-		//socket.emit('welcomePlayer', {name: username}); // emits welcome player and passes in the username
+		var username = $('#username').val(); // username of the player
+		var playerType = $("input[name=player]:checked").val();
+		socket.emit('chooseGameAndCategory', {player: playerType});
 	});
+
+	socket.on('choseGame', function(data){
+		$('.initialPage').fadeOut();
+		$('.spinGame').fadeIn();
+    });
+
+	socket.on('choseCategory', function(data){
+		$('.initialPage').fadeOut();
+		$('.spinCategory').fadeIn();
+    });
+
+    $('#digit').click(function () {
+    	$('.spinGame').fadeOut();
+		$('.waitingForParent').fadeIn();
+		socket.emit('gameSelected', {game: "digit"});    	
+    });
+
+    $('#home').click(function () {
+    	$('.spinCategory').fadeOut();
+    	$('#category').text("home");
+		$('.inputInformation').fadeIn();
+		socket.emit('categorySelected', {category: "home"});  
+    });
+
+    $('#costSubmit').click(function () {
+    	var item =  $('#item :selected').text();
+    	var cost = $('#cost').val();
+    	var per = $("input[name=per]:checked").val();
+    	socket.emit('saveCost', {item: item, cost: cost, per: per});
+    });
+
+    socket.on('waitForTeen', function(data) {
+    	$('.inputInformation').fadeOut();
+    	$('.waitingForTeen').fadeIn();
+    });
+
+    socket.on('startGame', function(data) {
+    	$('#categoryName').text(data.category);
+    	$('#perValue').text(data.per);
+    	var missingID = data.missing;
+    	var digitArray = data.digits;
+  		var element;
+    	for(var i=0; i<digitArray.length; i++) {
+    		if(i != missingID) {
+    			element = digitArray[i];
+    		} else {
+    			element = "_"
+    		}
+
+    		$('.digitList ul').append('<li>' + element + '</li>');
+    	}
+
+    	var missingDigitPlace = Math.floor((Math.random() * 2) + 1);
+    	var seenDigit = [];
+    	seenDigit.push(digitArray[missingID]);
+    	for(var i=0; i<3; i++) {
+    		if(i == missingDigitPlace){
+    			element = digitArray[missingID];
+    		} else {
+    			element = Math.floor((Math.random() * 9) + 1);
+    			while(seenDigit.indexOf(element) != -1){
+    				element = Math.floor((Math.random() * 9) + 1);
+    			}
+    			seenDigit.push(element);
+    		}
+
+    		$('.choices').append('<input type="radio" name="choice" value=' + element + '> ' + element);
+    	}
+
+    	$('.waitingForParent').fadeOut();
+    	$('.missingDigitScreen').fadeIn();
+    });
+
+	
+	$('#guess').click(function () {
+		var guess = $("input[name=choice]:checked").val();
+		socket.emit('checkGuess', {guess: guess})
+	});
+
+	socket.on('parentWin', function (data) {
+		$('.parentWin').modal()
+	});
+
+	socket.on('teenWin', function (data) {
+		$('.teenWin').modal()
+	});
+
+
+
+
 
 
 });
--- socketio/serverSocket.js
+++ socketio/serverSocket.js
@@ -1,28 +1,62 @@
 var Player = require('../models/player.js');
 
 exports.init = function(io) {
-	
-	// sends heartbeat so that the server does not disconnect automatically
-	function sendHeartbeat(){
-	    setTimeout(sendHeartbeat, 8000);
-	    io.sockets.emit('ping', { beat : 1 });
-	}
 
+	var parentID;
+	var teenID;
+	var game = "";
+	var category = "";
+	var item;
+	var cost;
+	var per;
+
+	// variables for missing digit
+	var digitArray;
+	var missingId;
+	
     // When a new connection is initiated
 	io.sockets.on('connection', function (socket) {
 		console.log('Connection started');
 
-		// sends heartbeat so that the server does not disconnect automatically
-		socket.on('pong', function(data){
-	        console.log("Pong received from client");
-	    });
-	    setTimeout(sendHeartbeat, 8000);
-
-
 		// When a new user joins the game
-		socket.on('welcomePlayer', function (data) {
-			console.log('Player Joined');
+		socket.on('chooseGameAndCategory', function (data) {
+			if(data.player == "parent"){
+				parentID = socket.id;
+				io.sockets.connected[parentID].emit('choseCategory');
+			} else {
+				teenID = socket.id;
+				io.sockets.connected[teenID].emit('choseGame');
+			}
 		}); 
+
+
+		socket.on('gameSelected', function (data) {
+			game = data.game;
+		});
+
+		socket.on('categorySelected', function (data) {
+			category = data.category;
+		});
+
+		socket.on('saveCost', function (data) { 
+			item = data.item;
+			cost = data.cost;
+			per = data.per;
+
+			digitArray = cost.toString(10).split("").map(function(t){return parseInt(t)});
+			missingId = Math.floor((Math.random() * (digitArray.length-1)) + 1);
+
+			io.sockets.connected[teenID].emit('startGame', {digits: digitArray, missing: missingId, per: per, category: category});
+			io.sockets.connected[parentID].emit('waitForTeen');
+		});
+
+		socket.on('checkGuess', function (data) {
+			if(data.guess == digitArray[missingId]) {
+				io.sockets.connected[teenID].emit('teenWin');
+			} else {
+				io.sockets.connected[teenID].emit('parentWin');
+			}
+		});
 
 	});
 }
